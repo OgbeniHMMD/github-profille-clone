702a7cbcb57979d22a8acf51d3b49cc9a4286b5f
OgbeniHMMD <olajide.a.hammed@gmail.com>
2021-02-18 00:27:51 +0100

    Improvemement

--- components/repository/MainList.vue
+++ components/repository/MainList.vue
@@ -1,7 +1,7 @@
 <template>
   <main>
     <article v-for="repo in repos" :key="repo.id" class="repo-article">
-      <div class=" ">
+      <div v-if="!repo.private && !repo.archived">
         <h3 class="title">
           <a :href="html_url"> {{ repo.name }}</a>
         </h3>
@@ -71,7 +71,7 @@
               </a>
             </span>
 
-            <span class="span">
+            <span class="span" v-if="repo.license">
               <svg
                 class="octicon octicon-law mr-1"
                 viewBox="0 0 16 16"
@@ -85,27 +85,17 @@
                   fill="grey"
                   fill-rule="evenodd"
                   d="M8.75.75a.75.75 0 00-1.5 0V2h-.984c-.305 0-.604.08-.869.23l-1.288.737A.25.25 0 013.984 3H1.75a.75.75 0 000 1.5h.428L.066 9.192a.75.75 0 00.154.838l.53-.53-.53.53v.001l.002.002.002.002.006.006.016.015.045.04a3.514 3.514 0 00.686.45A4.492 4.492 0 003 11c.88 0 1.556-.22 2.023-.454a3.515 3.515 0 00.686-.45l.045-.04.016-.015.006-.006.002-.002.001-.002L5.25 9.5l.53.53a.75.75 0 00.154-.838L3.822 4.5h.162c.305 0 .604-.08.869-.23l1.289-.737a.25.25 0 01.124-.033h.984V13h-2.5a.75.75 0 000 1.5h6.5a.75.75 0 000-1.5h-2.5V3.5h.984a.25.25 0 01.124.033l1.29.736c.264.152.563.231.868.231h.162l-2.112 4.692a.75.75 0 00.154.838l.53-.53-.53.53v.001l.002.002.002.002.006.006.016.015.045.04a3.517 3.517 0 00.686.45A4.492 4.492 0 0013 11c.88 0 1.556-.22 2.023-.454a3.512 3.512 0 00.686-.45l.045-.04.01-.01.006-.005.006-.006.002-.002.001-.002-.529-.531.53.53a.75.75 0 00.154-.838L13.823 4.5h.427a.75.75 0 000-1.5h-2.234a.25.25 0 01-.124-.033l-1.29-.736A1.75 1.75 0 009.735 2H8.75V.75zM1.695 9.227c.285.135.718.273 1.305.273s1.02-.138 1.305-.273L3 6.327l-1.305 2.9zm10 0c.285.135.718.273 1.305.273s1.02-.138 1.305-.273L13 6.327l-1.305 2.9z"
-                ></path></svg
-              >MIT License
+                ></path>
+              </svg>
+              {{ repo.license.name }}
             </span>
 
             <span class="span">
-              Updated
-              <time datetime="2021-01-18T09:47:43Z" class="no-wrap"
-                >Jan 18, 2021</time
-              >
+              Updated on
+              <time>{{ new Date(repo.updated_at).toDateString() }}</time>
             </span>
           </div>
         </nav>
-      </div>
-
-      <div class="col-2 col-lg-3 d-flex flex-column flex-justify-around">
-        <!-- <div class="text-right hide-lg hide-md hide-sm hide-xs mt-2">
-          <poll-include-fragment
-            src="/OgbeniHMMD/chat-app/graphs/participation?w=155&amp;h=28&amp;type=sparkline)"
-          >
-          </poll-include-fragment>
-        </div> -->
       </div>
     </article>
   </main>
