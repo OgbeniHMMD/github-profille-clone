26d97a6543ba93e866539b605b32e2be7644d7e1
OgbeniHMMD <olajide.a.hammed@gmail.com>
2021-02-18 01:28:56 +0100

    Improved User

--- components/repository/MainList.vue
+++ components/repository/MainList.vue
@@ -87,29 +87,14 @@ export default {
     return {
       USERNAME: 'OgbeniHMMD',
       repos: [],
-      userData: [],
     }
   },
 
   created() {
-    this.fetchUserInfo()
     this.fetchRepos()
   },
 
   methods: {
-    async fetchUserInfo() {
-      const URI = `/users/${this.USERNAME}/repos`
-      await this.$axios
-        .$get(URI)
-        .then((response) => {
-          console.log(response)
-          this.repos = response
-        })
-        .catch((error) => {
-          console.log(error)
-        })
-    },
-
     async fetchRepos() {
       const URI = `/users/${this.USERNAME}/repos`
       await this.$axios
